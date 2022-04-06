67f87b9a5c405cc5dd9834a6804c6b14b635cdf0
Michael <liumichael101@gmail.com>
2022-04-05 19:11:14 -0700

    add most html and some JS variables

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+var startBtn = document.getElementById("start")
+var codeQs = document.getElementById("questions")
+var firstChoice = document.getElementById("answer1")
+var secondChoice = document.getElementById("answer2")
+var thirdChoice = document.getElementById("answer3")
+var fourthChoice = document.getElementById("answer4")
