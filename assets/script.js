16e96faabb1e0eef7305dbc23cdf0f90b12d58b3
Michael <liumichael101@gmail.com>
2022-04-05 23:06:00 -0700

    add score div and some css

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,9 @@
 var startBtn = document.getElementById("start")
-var codeQs = document.getElementById("questions")
+var questionEl = document.getElementById("questions")
 var firstChoice = document.getElementById("answer1")
 var secondChoice = document.getElementById("answer2")
 var thirdChoice = document.getElementById("answer3")
 var fourthChoice = document.getElementById("answer4")
-var clock = document.getElementById("timer")
+var timerEl = document.getElementById("timer")
 
+var codeQs = ["What is a JavaScript function?", "How many data types are in JavaScript?", "How many different types of objects are in JavaScript?", "Which of these data types cannot contain values?", ""]
