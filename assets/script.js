98bbc26ea04f21c6d4ec637cf56177fb3ff894bb
Michael <liumichael101@gmail.com>
2022-04-06 00:01:07 -0700

    edit js

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ var thirdChoice = document.getElementById("answer3");
 var fourthChoice = document.getElementById("answer4");
 var timerEl = document.getElementById("timer");
 
+var currentQs = "";
 var codeQs = ["What is a JavaScript function?", "How many data types are in JavaScript?", "How many different types of objects are in JavaScript?", "Which of these data types cannot contain values?", "Which HTML element do we put the JavaScript in?"];
 var a1 = ["A JavaScript party.", "A block of code designed to perform a task.", "An array", "A standalone entity, with properties and type."];
 var a2 = [10, 15, 5, 7];
@@ -15,3 +16,8 @@ var a5 = ["<head>", "<body>", "<script>", "<section>"];
 
 var countdown = 60;
 
+function startTimer(event) {
+    event.preventDefault();
+    startBtn.setAttribute("style", "display: none");
+    
+}
